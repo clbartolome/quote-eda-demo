a11de95785d134e3b02e7e31c852cc494937b26a
Donato Marrazzo <donato.marrazzo@gmail.com>
2024-05-17 11:28:30 +0200

    logging

--- amqp-quickstart-processor/src/main/java/org/acme/amqp/processor/QuoteProcessor.java
+++ amqp-quickstart-processor/src/main/java/org/acme/amqp/processor/QuoteProcessor.java
@@ -2,13 +2,13 @@ package org.acme.amqp.processor;
 
 import java.util.Random;
 
-import jakarta.enterprise.context.ApplicationScoped;
-
 import org.acme.amqp.model.Quote;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.jboss.logging.Logger;
 
 import io.smallrye.reactive.messaging.annotations.Blocking;
+import jakarta.enterprise.context.ApplicationScoped;
 
 /**
  * A bean consuming data from the "request" AMQP queue and giving out a random quote.
@@ -16,6 +16,7 @@ import io.smallrye.reactive.messaging.annotations.Blocking;
  */
 @ApplicationScoped
 public class QuoteProcessor {
+    private static final Logger LOG = Logger.getLogger(QuoteProcessor.class);
 
     private Random random = new Random();
 
@@ -23,6 +24,7 @@ public class QuoteProcessor {
     @Outgoing("quotes")         // quotes
     @Blocking(ordered = false)  // blocking method
     public Quote process(String quoteRequest) throws InterruptedException {
+        LOG.info("quote serving");
         // simulate some hard working task
         Thread.sleep(200+random.nextInt(10)*200);
         return new Quote(quoteRequest, random.nextInt(100));
--- amqp-quickstart-producer/src/main/java/org/acme/amqp/producer/QuotesResource.java
+++ amqp-quickstart-producer/src/main/java/org/acme/amqp/producer/QuotesResource.java
@@ -2,20 +2,22 @@ package org.acme.amqp.producer;
 
 import java.util.UUID;
 
+import org.acme.amqp.model.Quote;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.jboss.logging.Logger;
+
+import io.smallrye.mutiny.Multi;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-import org.acme.amqp.model.Quote;
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
-
-import io.smallrye.mutiny.Multi;
-
 @Path("/quotes")
 public class QuotesResource {
+
+    private static final Logger LOG = Logger.getLogger(QuotesResource.class);
 
     // Inject a Reactive Messaging Emitter to send messages to the quote-requests channel
     @Channel("quote-requests")
@@ -32,6 +34,7 @@ public class QuotesResource {
     @GET
     @Produces(MediaType.SERVER_SENT_EVENTS) // Indicates that the content is sent using Server Sent Events
     public Multi<Quote> stream() {
+        LOG.info("stream for SSE");
         return quotes; // Returns the (reactive) stream which is fed by the quote channel
     }
 
@@ -43,6 +46,7 @@ public class QuotesResource {
     @Path("/request")
     @Produces(MediaType.TEXT_PLAIN)
     public String createRequest() {
+        LOG.info("new quote request");
         UUID uuid = UUID.randomUUID();                 // On a post request, generate a random UUID and
         quoteRequestEmitter.send(uuid.toString()); // send it to the quote request queue using the emitter
         return uuid.toString();
