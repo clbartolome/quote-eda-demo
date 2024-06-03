203f15d4acc2bc12d294bcf7c290e2bc2fd055a3
Donato Marrazzo <donato.marrazzo@gmail.com>
2024-06-03 10:38:11 +0200

    word spitter

--- quote-analytics/src/main/java/com/redhat/demo/kstreams/TopologyProducer.java
+++ quote-analytics/src/main/java/com/redhat/demo/kstreams/TopologyProducer.java
@@ -1,8 +1,13 @@
 package com.redhat.demo.kstreams;
 
+import java.util.Arrays;
+
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
@@ -14,8 +19,11 @@ public class TopologyProducer {
     public Topology buildTopology() {
         StreamsBuilder builder = new StreamsBuilder();
 
-        KStream<String, String> source = builder.stream("streams-plaintext-input");
-        source.to("streams-pipe-output");
+        KStream<String, String> source = builder.stream("streams-plaintext-input", Consumed.with(Serdes.String(), Serdes.String()));
+
+        KStream<String, String> words = source.flatMapValues(value -> Arrays.asList(value.split("\\W+")));
+
+        words.to("streams-pipe-output", Produced.with(Serdes.String(), Serdes.String()));
 
         var topology = builder.build();
 
